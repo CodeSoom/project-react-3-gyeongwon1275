aa6fd1a8511dac558931aa754868d2f332c62c37
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-03-29 17:09:50 +0900

    Change sequelize models to start with uppercase letters
    - since the sequelize class starts with an uppercase letter,
    - the file name is also changed to start with an uppercase letter.

---
+++ back/models/User.js
@@ -0,0 +1,55 @@
+const { DataTypes, Model } = require('sequelize');
+
+module.exports = class User extends Model {
+  static init(sequelize) {
+    return super.init(
+      {
+        user_id: {
+          type: DataTypes.STRING(15),
+          allowNull: false,
+          unique: true,
+        },
+        password: {
+          type: DataTypes.STRING(70),
+          allowNull: false,
+          unique: false,
+        },
+        name: {
+          type: DataTypes.STRING(15),
+          allowNull: false,
+          unique: false,
+        },
+        phone: {
+          type: DataTypes.STRING(15),
+          allowNull: false,
+          unique: true,
+        },
+        email: {
+          type: DataTypes.STRING(30),
+          allowNull: false,
+          unique: true,
+        },
+        created_at: {
+          type: DataTypes.DATE,
+          allowNull: false,
+          defaultValue: DataTypes.NOW,
+        },
+      },
+      {
+        underscored: false,
+        timestamps: false,
+        modelName: 'user',
+        tableName: 'users',
+        charset: 'utf8',
+        collate: 'utf8_general_ci',
+        sequelize,
+      },
+    );
+  }
+
+  static associate(db) {
+    db.User.hasMany(db.Comment);
+    db.User.hasMany(db.Post);
+    db.User.belongsToMany(db.Post, { through: 'Like' });
+  }
+};
