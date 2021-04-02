db40effb73073f155df73e1a2fe165ada491072e
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-04-02 12:01:45 +0900

    Change webpack.config.js
    - add folders to exclude __tests__, __mocks__

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -11,7 +11,7 @@ module.exports = (env) => ({
     rules: [
       {
         test: /\.jsx?$/,
-        exclude: /node_modules/,
+        exclude: /(node_modules|__tests__|__mocks__)/,
         use: 'babel-loader',
       },
       {
