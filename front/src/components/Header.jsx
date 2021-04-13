246b55bd76319e950b4e746e130db4cb41244665
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-04-13 21:28:00 +0900

    Change Header logo, upload button css
    - Set desktop and mobile version of css separately

--- front/src/components/Header.jsx
+++ front/src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { useDispatch } from 'react-redux';
 
-import { PlusCircleFilled } from '@ant-design/icons';
+import { UploadOutlined, PlusOutlined } from '@ant-design/icons';
 
 import styled from '@emotion/styled';
 
@@ -11,18 +11,90 @@ import { setFormVisible } from '../data/postReducer';
 import PostFormContainer from '../container/PostFormContainer';
 import LoginButtonContainer from '../container/LoginButtonContainer';
 
-const PlusIcon = styled(PlusCircleFilled)({
-  fontSize: '2rem',
-  color: '#333',
-  backgroundColor: '#fff',
-  borderRadius: '1rem',
+const DesktopUploadButton = styled.button({
+  '@media (max-width: 768px)': {
+    display: 'none',
+  },
+
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+
+  margin: '0 10px',
+  padding: '6.4px 15px',
+
+  width: 'auto',
+  height: '40px',
+
+  backgroundColor: '#6157ff',
+  borderRadius: '2px',
+
+  cursor: 'pointer',
+
+  '&:hover': {
+    backgroundColor: '#473fd0',
+  },
+
+  transition: 'all 0.3s',
 });
 
-const StyledHeadr = styled.div({
+const MobileUploadButton = styled.button({
+  '@media (min-width: 768px)': {
+    display: 'none',
+  },
+  marginRight: '10px',
 
+  height: '40px',
+
+  fontSize: '1.7rem',
+  fontWeight: 'bold',
+
+  cursor: 'pointer',
+});
+
+const UploadIcon = styled(UploadOutlined)({
+  fontSize: '1.5rem',
+  marginRight: '5px',
+});
+
+const HeaderWrapper = styled.div({
   display: 'flex',
   justifyContent: 'flex-start',
 
+  flex: '1 1 auto',
+
+  height: 'auto',
+});
+
+const LogoWrapper = styled.div({
+
+  fontSize: '1.5rem',
+  fontWeight: 'bold',
+
+  display: 'flex',
+  justifyContent: 'flex-start',
+  alignItems: 'center',
+
+  flex: '8 8 auto',
+
+  '@media (min-width: 768px)': {
+    marginLeft: '5px',
+
+    flex: '7 7 auto',
+  },
+});
+
+const UploadButtonWrapper = styled.div({
+
+  display: 'flex',
+  justifyContent: 'flex-end',
+  alignItems: 'center',
+
+  flex: '2 2 auto',
+
+  '@media (min-width: 768px)': {
+    flex: '3 3 auto',
+  },
 });
 
 export default function Header() {
@@ -33,15 +105,19 @@ export default function Header() {
   };
 
   return (
-    <StyledHeadr>
-      <div className="logo-box">AnimalPhy</div>
-      <div className="upload-box">
-        <button type="button" onClick={openPostForm}>
-          <PlusIcon />
-        </button>
-      </div>
+    <HeaderWrapper>
+      <LogoWrapper>AnimalPhy</LogoWrapper>
+      <UploadButtonWrapper>
+        <DesktopUploadButton type="button" onClick={openPostForm}>
+          <UploadIcon />
+          <span>Upload</span>
+        </DesktopUploadButton>
+        <MobileUploadButton type="button" onClick={openPostForm}>
+          <PlusOutlined />
+        </MobileUploadButton>
+      </UploadButtonWrapper>
       <LoginButtonContainer />
       <PostFormContainer />
-    </StyledHeadr>
+    </HeaderWrapper>
   );
 }
