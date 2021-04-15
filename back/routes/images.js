f84d904f3f7ba64251119990d85101ad5354965a
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-04-15 17:49:36 +0900

    Change the response of getImages router
    - change the response to send an image registered earlier than the lastId image

--- back/routes/images.js
+++ back/routes/images.js
@@ -1,12 +1,21 @@
 const express = require('express');
+
+const { Op } = require('sequelize');
+
 const router = express.Router();
 
 const { Image } = require('../models');
 
 router.get('/', async (request, response, next) => {
+
   try {
+    const lastId = parseInt(request.query.lastId, 10)
+
+    const where = lastId ? { id: { [Op.lt]: lastId } } : null
+
     const images = await Image.findAll({
       limit: 10,
+      where,
       order: [['created_at', 'DESC']],
     });
 
